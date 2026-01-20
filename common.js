810fa3ac4d4a1e69fba64c0e34e5d373828d557a
ShadowCryptic <shadowcryptic73@gmail.com>
2026-01-20 12:13:55 +1100

    Big Changes

--- common.js
+++ common.js
@@ -1,7 +1,20 @@
-<script type="module">
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-  import { getFirestore, doc, setDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  onSnapshot,
+  runTransaction,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDQcaTRaafEoKNYiTpElLWRa9cfpiP0y5w",
   authDomain: "remote-planechase.firebaseapp.com",
@@ -11,31 +24,14 @@ const firebaseConfig = {
   appId: "1:533208797807:web:795c57b0fdee32ae05339a"
 };
 
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 
-  window.firebaseState = {
-    db,
-    doc,
-    setDoc,
-    onSnapshot
-  };
-</script>
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDQcaTRaafEoKNYiTpElLWRa9cfpiP0y5w",
-//   authDomain: "remote-planechase.firebaseapp.com",
-//   projectId: "remote-planechase",
-//   storageBucket: "remote-planechase.firebasestorage.app",
-//   messagingSenderId: "533208797807",
-//   appId: "1:533208797807:web:795c57b0fdee32ae05339a"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+export {
+  doc,
+  setDoc,
+  onSnapshot,
+  runTransaction,
+  serverTimestamp
+};
